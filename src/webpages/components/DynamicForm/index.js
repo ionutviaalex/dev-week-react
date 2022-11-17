053475d4b4799de65f52549b9498c176b4ac2ad3
root <ionutalexmail@gmail.com>
2022-11-18 00:04:39 +0200

    Improvements

---
+++ src/webpages/components/DynamicForm/index.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+export default class DynamicForm extends React.Component {
+  state = {};
+  constructor(props) {
+    super(props);
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    let derivedState = {};
+
+    if (
+      nextProps.defaultValues &&
+      nextProps.defaultValues.id !== prevState.id
+    ) {
+      return {
+        ...nextProps.defaultValues
+      };
+    }
+    return null;
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.props.onSubmit) this.props.onSubmit(this.state);
+  };
+
+  onChange = (e, key, type = "single") => {
+    //console.log(`${key} changed ${e.target.value} type ${type}`);
+    if (type === "single") {
+      this.setState(
+        {
+          [key]: e.target.value
+        },
+        () => {}
+      );
+    }
+  };
+
+  renderForm = () => {
+    let model = this.props.model;
+    let defaultValues = this.props.defaultValues;
+
+    let formUI = model.map(m => {
+      let key = m.key;
+      let type = m.type || "text";
+      let props = m.props || {};
+      let name = m.name;
+      let value = m.value;
+
+      let target = key;
+      value = this.state[target] || "";
+
+      let input = (
+        <input
+          {...props}
+          className="form-input"
+          type={type}
+          key={key}
+          name={name}
+          value={value}
+          onChange={e => {
+            this.onChange(e, target);
+          }}
+        />
+      );
+
+      return (
+        <div key={"g" + key} className="form-group">
+          <label className="form-label" key={"l" + key} htmlFor={key}>
+            {m.label}
+          </label>
+          {input}
+        </div>
+      );
+    });
+    return formUI;
+  };
+
+  render() {
+    let title = this.props.title || "Dynamic Form";
+
+    return (
+      <div className={this.props.className}>
+        <h3 className="form-title">{title}</h3>
+        <form
+          className="dynamic-form"
+          onSubmit={e => {
+            this.onSubmit(e);
+          }}
+        >
+          {this.renderForm()}
+          <div className="form-actions">
+            <button type="submit">submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
--- src/webpages/home.js
+++ src/webpages/home.js
@@ -1,73 +1,103 @@
 import React, { useState, useEffect }  from 'react';
+import DynamicForm from "./components/DynamicForm";
+
 const Home = () => {
 	const [statusMessage1, setStatusMessage1] = useState("TBD");
 	const [statusMessage2, setStatusMessage2] = useState("TBD");
-	const [getReturn, setGetReturn] = useState("Empty");
-	const [postReturn, setPostReturn] = useState("Empty");
-	const [postInput, setPostInput] = useState('');
+	const [statusMessage3, setStatusMessage3] = useState("TBD");
+	const [studentGetReturn, setStudentGetReturn] = useState("Empty");
+	const [studentPostReturn, setStudentPostReturn] = useState("Empty");
+	const [currentTime, setCurrentTime] = useState("Unknown Time");
 
-	const url = "https://cors-everywhere.herokuapp.com/" + process.env.REACT_APP_API_URL + "/api/";
-	const getUrl = url + "time";
-	const postUrl = url + "json";
 
-	//GET Request using fetch with basic error handling
-	const getData = () => {
-		fetch(getUrl)
+    const simpleUrl = "https://cors-everywhere.herokuapp.com/" + process.env.REACT_APP_API_URL;
+	const healthUrl = simpleUrl + "/";
+	const timeUrl = simpleUrl + "/time";
+	const studentUrl = simpleUrl + "/api/";
+
+	//GET Time Request, endpoint returns text
+	const getTime = () => {
+		fetch(timeUrl)
 		.then(r => r.text())
 		.then((response) => {
-			console.log("GET response:", response);
-			setGetReturn(response);
+			console.log("GET Time response:", response);
+			setCurrentTime(response);
 			setStatusMessage1("SUCCESS!");
 		})
 		.catch(error => {
 			setStatusMessage1(error.toString());
-			console.error('There was an error!', error);
+			console.error('There was an error while getting the time!', error);
 		});
 	};
-  
-    // POST request using fetch with error handling
-	const handlePostInputChange = (event) => {setPostInput(event.target.value)};
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/text' },
-        body: postInput
+
+    //GET Students Request, endpoint returns JSON with list of students
+    const getStudents = () => {
+        fetch(studentUrl)
+        .then(r => r.json())
+        .then((response) => {
+            console.log("GET Students response:", response);
+            setStudentGetReturn(JSON.stringify(response));
+            setStatusMessage2("SUCCESS!");
+        })
+        .catch(error => {
+            setStatusMessage2(error.toString());
+            console.error('There was an error while getting the students!', error);
+        });
     };
-    const postData = () => {
-		fetch(postUrl, requestOptions)
+
+    // POST Student request, endpoint return JSON with the saved student
+    const onStudentSubmit = model => {
+        console.log("This is the JSON:", JSON.stringify(model));
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(model)
+        };
+        fetch(studentUrl, requestOptions)
         .then(async response => {
-			console.log("POST request made with options:", requestOptions);
+            console.log("POST Student request made with options:", requestOptions);
             const data = await response.json();
 
             // check for error response
             if (!response.ok) {
-				console.log("A wild error appeared!");
+                console.log("A wild error appeared!");
                 // get error message from body or default to response status
                 const error = (data && data.message) || response.status;
                 return Promise.reject(error);
             }
 
-			console.log("POST response:", response);
-			console.log("POST JSON:", data);
-			setStatusMessage2("SUCCESS!");
-            setPostReturn(JSON.stringify(data));
+            console.log("POST response:", response);
+            console.log("POST JSON:", data);
+            setStatusMessage3("SUCCESS!");
+            setStudentPostReturn(JSON.stringify(data));
         })
         .catch(error => {
-			setStatusMessage2(error.toString());
-			console.error('There was an error!', error);
+            setStatusMessage3(error.toString());
+            console.error('There was an error while posting a student!', error);
         });
-	};	
-		
+    };
+
 return(
         <div>
-			<button variant="contained" onClick={getData}>GET</button>
+            <button onClick={getTime}>GET Time</button>
+            <h3>{currentTime}</h3>
             <h3>API Status: {statusMessage1}</h3>
-			<h3>GET return:{getReturn}</h3>
-			<br/>
-			
-			<input type="text" id="post-request-input-message" onChange={handlePostInputChange} value={postInput}></input>
-			<button variant="contained" onClick={postData}>POST</button>
-			<h3>API Status: {statusMessage2}</h3>
-			<h3>POST return:{postReturn}</h3>
+            <br/><br/><br/>
+
+			<button onClick={getStudents}>GET Students</button>
+            <h3>API Status: {statusMessage2}</h3>
+			<h3>GET return:{studentGetReturn}</h3>
+			<br/><br/><br/>
+
+			<DynamicForm title="Student"
+			    model={[
+			        {key:"id", label:"Id", props:{required:true}},
+			        {key:"name", label:"Name"},
+			        {key:"age", label:"Age", type:"number"},
+			    ]}
+			    onSubmit = {(model) => {onStudentSubmit(model)}}
+			/>
+			<h3>API Status: {statusMessage3}</h3>
         </div>
     );
 }
